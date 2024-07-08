1aca9cc22345159691a0480b341f0cdd521443c8
Okoh Joachim <joachimokoh23@gmail.com>
2024-07-08 16:24:37 +0100

    new fixes

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -66,7 +66,7 @@ const config = {
         hashed: true,
         searchBarPosition: "auto",
         searchBarShortcut: true,
-        indexDocs: true
+        // indexDocs: true
       }),
     ],
     [
