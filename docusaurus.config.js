b8a20bc987d5ddf21ca2d8ec7eccb7edee15d633
odelola <integralandintegrations@gmail.com>
2024-08-21 11:46:52 +0100

    fix: reworked on the searhbar

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -130,7 +130,7 @@ const config = {
             position: 'left',
             label: 'Docs'
           },
-          {to: '/api/intro', label: 'APIs', position: 'left'},
+          {to: '/api/intro', label: 'API', position: 'left'},
           {
             type: 'dropdown',
             label: 'Community',
