ea3a9feb494ebc7785f2703f38aed4c1ef714009
Okoh Joachim <joachimokoh23@gmail.com>
2024-08-22 12:19:11 +0100

    chore: final documentation updates

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -56,7 +56,10 @@ const config = {
           //   'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         theme: {
-          customCss: [require.resolve('./src/css/custom.scss')],
+          customCss: [
+            require.resolve('./src/css/custom.scss'),
+            require.resolve('./node_modules/modern-normalize/modern-normalize.css')
+          ],
         },
       }),
     ],
